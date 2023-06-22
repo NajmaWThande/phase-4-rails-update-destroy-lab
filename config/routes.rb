7615234a95db81577827c1eee8234d299d662453
Najma <najmathande@gmail.com>
2023-06-22 17:48:54 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # ...
+  resources :plants, only: [:show, :update, :destroy]
+  # ...
 end
